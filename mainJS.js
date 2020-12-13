5ff8ba6adc297ff2c88937c60dc88f48f3de4b98
TianyouZ <tianyouzhang@outlook.com>
2020-12-13 09:50:07 +0800

    Near completion

--- mainJS.js
+++ mainJS.js
@@ -1,5 +1,12 @@
-document.getElementById("Promptt").innerHTML = "Showing entry No. " + localStorage.getItem("storedIdx");        
-	
+
+	if (localStorage.getItem("storedIdx") === null || localStorage.getItem("storedList") === null || localStorage.getItem("totalLen") === null){
+		localStorage.setItem("storedIdx", "0");
+		localStorage.setItem("storedList", JSON.stringify(["Cover Page"]));
+		localStorage.setItem("totalLen", "1");
+	}
+	if (localStorage.getItem("storedIdx") === "0") {document.getElementById("Promptt").innerHTML = "Showing the cover page!";}
+	else {document.getElementById("Promptt").innerHTML = "Showing Entry No. " + localStorage.getItem("storedIdx");}        
+
 	var list = JSON.parse(localStorage.storedList);
 	localStorage.setItem("storedList", JSON.stringify(list));
         
@@ -7,27 +14,19 @@ document.getElementById("Promptt").innerHTML = "Showing entry No. " + localStora
 	localStorage.setItem("totalLen", list.length);
 	document.getElementById("fentry").value =  (JSON.parse(localStorage.getItem("storedList")))[parseInt(localStorage.getItem("storedIdx"))];
 
-	function init(){
-		list = ["Saab", "Volvo", "BMW"];
-		localStorage.setItem("storedList", JSON.stringify(list));
-        
-	
-	localStorage.setItem("totalLen", list.length);
-	document.getElementById("fentry").value =  (JSON.parse(localStorage.getItem("storedList")))[parseInt(localStorage.getItem("storedIdx"))];
-	}
-
 	function prev(){
 		if (parseInt(localStorage.getItem("totalLen")) !== 0){
 		localStorage.setItem("storedIdx", JSON.stringify(  (parseInt(localStorage.getItem("storedIdx")) + (parseInt(localStorage.totalLen))- 1) % (parseInt(localStorage.totalLen)) ));
-		document.getElementById("Promptt").innerHTML = "Showing entry No. " + localStorage.getItem("storedIdx");
+		if (localStorage.getItem("storedIdx") === "0") {document.getElementById("Promptt").innerHTML = "Showing the cover page!";}
+		else {document.getElementById("Promptt").innerHTML = "Showing Entry No. " + localStorage.getItem("storedIdx");}   
 		document.getElementById("fentry").value =  (JSON.parse(localStorage.getItem("storedList")))[parseInt(localStorage.getItem("storedIdx"))];
 		}
 		
 	}
 	function next(){
-		
 		localStorage.setItem("storedIdx", JSON.stringify(  (parseInt(localStorage.getItem("storedIdx")) + 1) % (parseInt(localStorage.totalLen)) ));
-		document.getElementById("Promptt").innerHTML = "Showing entry No. " + localStorage.getItem("storedIdx");
+		if (localStorage.getItem("storedIdx") === "0") {document.getElementById("Promptt").innerHTML = "Showing the cover page!";}
+		else {document.getElementById("Promptt").innerHTML = "Showing Entry No. " + localStorage.getItem("storedIdx");}   
 		document.getElementById("fentry").value =  (JSON.parse(localStorage.getItem("storedList")))[parseInt(localStorage.getItem("storedIdx"))];
 	}
 	function newEntryFun(){
